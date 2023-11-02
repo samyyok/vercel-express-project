6479224b6b8edbf8a579fb768aae3d7005e4244a
samyyok <samyoksamba17@gmail.com>
2023-11-03 00:59:18 +0100

    first commit

--- myapp/app.js
+++ myapp/app.js
@@ -49,4 +49,25 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+
+
+
+
+const PORT = 4000
+
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+})
+
+app.get('/about', (req, res) => {
+  res.send('This is my about route..... ')
+})
+
+// Export the Express API
+module.exports = app
+
+
