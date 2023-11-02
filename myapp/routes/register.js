f455add8752c0eb7b5081b0aae64310089e98d65
samyyok <samyoksamba17@gmail.com>
2023-11-02 19:09:23 +0100

    first commit

---
+++ myapp/routes/register.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+// router.post('/user',(req,res)=>{
+//   const body = req.body
+//   res.json({
+//     confirmation:'success',
+//     route:'register',
+//     data:body
+//   })
+
+// })
+
+module.exports=router
